bc43dc74bf07517d2fc6dff8008e4f9a9652117d
codermarvin <hiemmarvin@gmail.com>
2024-12-24 14:13:11 +0800

    update hlc cdl

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3663741227"
+    serviceWorkerVersion: "1999277451"
   }
 });
