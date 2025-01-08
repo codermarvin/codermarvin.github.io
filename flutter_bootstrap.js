c08caf5489fcf6e4c8fd709e8f706992f093fb75
codermarvin <hiemmarvin@gmail.com>
2025-01-08 17:12:26 +0800

    update hlc cdl

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "288649240"
+    serviceWorkerVersion: "1767726561"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f25b22e0863c10d20b704763f90dd21f",
+const RESOURCES = {"flutter_bootstrap.js": "e8ddbde30ae0bc81649179b3e47eaa6c",
 "version.json": "c37d0128d9641fbec0325663f1e0085f",
 "index.html": "d0eb49b5be4ecb43bbd0330133ea39ab",
 "/": "d0eb49b5be4ecb43bbd0330133ea39ab",
-"main.dart.js": "c0a96368e6207459a1f153aa60e1d8ae",
+"main.dart.js": "4585ea4300788a5c2614155c35c359c4",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
