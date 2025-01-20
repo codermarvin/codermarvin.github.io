f17fd2206330c9b011f582cda7ef25c7a9e67609
codermarvin <hiemmarvin@gmail.com>
2025-01-20 21:22:32 +0800

    update hlc cdl

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e8ddbde30ae0bc81649179b3e47eaa6c",
+const RESOURCES = {"flutter_bootstrap.js": "7ef2e0258db69f3e4911b051a22d9255",
 "version.json": "c37d0128d9641fbec0325663f1e0085f",
 "index.html": "d0eb49b5be4ecb43bbd0330133ea39ab",
 "/": "d0eb49b5be4ecb43bbd0330133ea39ab",
-"main.dart.js": "4585ea4300788a5c2614155c35c359c4",
+"main.dart.js": "ee53a5ea5602b7d4433fe4127db8db60",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
