67139ab647dd0e14bb86c04663a350c82fb8768d
codermarvin <hiemmarvin@gmail.com>
2025-06-17 05:07:42 -0700

    update hlc lookup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fbc7472331205c5a4b02016ca171fa10",
+const RESOURCES = {"flutter_bootstrap.js": "a004ccda3063cff472a076a9912c3276",
 "version.json": "c37d0128d9641fbec0325663f1e0085f",
 "index.html": "d0eb49b5be4ecb43bbd0330133ea39ab",
 "/": "d0eb49b5be4ecb43bbd0330133ea39ab",
-"main.dart.js": "456a33526948672810e8deea512485a9",
+"main.dart.js": "085263b98f091b1475b046e17e6c38e0",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
