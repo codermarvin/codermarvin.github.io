86a7e5da8f535023717d71341283e7457b497d7d
codermarvin <hiemmarvin@gmail.com>
2025-10-17 23:40:48 +0800

    add aca participants list

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1372926423"
+    serviceWorkerVersion: "2312139491"
   }
 });
